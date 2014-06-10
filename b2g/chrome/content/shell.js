d6483655a48275ae8892265d6d6e48962d6292da
Jan Keromnes <janx@linux.com>
2014-06-10 15:22:50 +0200

    Bug 1025804 - Implement an actor to live stream performances data from device to the app manager.

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -893,6 +893,7 @@ let RemoteDebugger = {
           globalActorFactories: restrictPrivileges ? {
             webappsActor: DebuggerServer.globalActorFactories.webappsActor,
             deviceActor: DebuggerServer.globalActorFactories.deviceActor,
+            monitorActor: DebuggerServer.globalActorFactories.monitorActor,
           } : DebuggerServer.globalActorFactories
         };
         let devtools = Cu.import("resource://gre/modules/devtools/Loader.jsm", {}).devtools;
