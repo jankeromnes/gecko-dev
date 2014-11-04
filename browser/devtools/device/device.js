b60b216af8783d0f2df38eecde10fcc561a4b6e0
Jan Keromnes <janx@linux.com>
2014-11-04 21:09:55 +0100

    Device panel.

---
+++ browser/devtools/device/device.js
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+
+/**
+ * The current target and the timeline front, set by this tool's host.
+ */
+let gToolbox, gTarget;
